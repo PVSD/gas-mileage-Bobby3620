1cf840d457320acd364beac59033cf41c110d942
Bobby3620 <41356385+Bobby3620@users.noreply.github.com>
2018-10-12 08:21:37 -0400

    na

--- src/com/company/Automobile.java
+++ src/com/company/Automobile.java
@@ -4,4 +4,30 @@ package com.company;
  * Created by dpennebacker on 10/9/16.
  */
 public class Automobile {
+
+    public int mpg;
+    public int gallons;
+    public int fillup;
+    public int taketrip;
+    public double reportfuel;
+
+    public Automobile(int a){
+        mpg = a;
+        gallons = 0;
+    }
+
+    public int fillUp(int b){
+        fillup = b + gallons;
+        return fillup;
+    }
+
+    public int takeTrip(int c){
+        taketrip = c - fillup;
+        return taketrip;
+    }
+
+    public double reportFuel(double d){
+        reportfuel = taketrip;
+        return reportfuel;
+    }
 }
